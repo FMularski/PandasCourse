00a6007b93377c328514b58df71017c714f3088b
Filip Mularski <mularskif@gmail.com>
2020-08-24 14:26:35 +0200

    missing values

---
+++ missing_values/01_missing.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import numpy as np
+
+
+df = pd.DataFrame(np.random.randn(10, 4),
+                  columns=['one', 'two', 'three', 'four'])
+
+# %% 
+
+for row in df.values:
+    switch = np.random.choice([0, 1])
+    
+    if switch:
+        i = np.random.choice([0, 1, 2, 3])
+        row[i] = np.nan
+        
+# %% isnull
+df.isnull()
+df[df.isnull()]
+
+print(df['one'].isnull())
+print(df[df['one'].isnull()])
+print(df[~df['one'].isnull()])
+
+# %% notnull
+
+print(df.notnull())
+print(df[df.notnull()])
+
+# %%
+
+df['five'] = np.nan
+
+# %%
+
+del df['five']
---
+++ missing_values/03_drop_nan.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import numpy as np
+
+
+df = pd.DataFrame(np.random.randn(10, 4),
+                  columns=['one', 'two', 'three', 'four'])
+
+for row in df.values:
+    switch = np.random.choice([0, 1])
+    
+    if switch:
+        i = np.random.choice([0, 1, 2, 3])
+        row[i] = np.nan
+        
+# %% 
+
+print(df.describe())
+
+# %%
+
+df = df.dropna(axis=0)  # axis = 0 usuwa po pierwszach, axis = 1 po kolumnach
+
+# %% 
