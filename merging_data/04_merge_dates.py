210e053cce89bb0aace5382aa0dc9d6b47c9c936
Filip Mularski <mularskif@gmail.com>
2020-08-27 11:26:15 +0200

    merge dates

---
+++ merging_data/04_merge_dates.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import numpy as np
+
+
+d1 = {'date':['2019-01-01', '2019-01-01', '2019-01-02', '2019-01-02'],
+      'id': ['0001', '0002', '0003', '0004'],
+      'product_id': ['0343', '5312', '5626', '9129']}
+
+d2 = {'date':['2019-01-01', '2019-01-01', '2019-01-02', '2019-01-03'],
+      'id': ['0001', '0002', '0003', '0004'],
+      'price': ['03', '532', '26', '929']}
+
+left = pd.DataFrame(d1)
+right = pd.DataFrame(d2)
+
+# %% inner
+
+df_inner = pd.merge(left, right, how='inner', on=['date', 'id'])
+
+# %% outer
+
+df_outer = pd.merge(left, right, how='outer', on=['date', 'id'])
+
+# %% left
+
+df_left = pd.merge(left, right, how='left', on=['date', 'id'])
+
+# %% right
+
+df_left = pd.merge(left, right, how='right', on=['date', 'id'])
